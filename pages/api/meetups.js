4329073a386acc283c33ec68c15e6fee7df048ad
franklysatoh <48499098+franklysatoh@users.noreply.github.com>
2021-08-04 16:56:18 +0700

    Format code
    Format code

--- pages/api/meetups.js
+++ pages/api/meetups.js
@@ -1,14 +1,14 @@
-import {MongoClient} from "mongodb";
+import { MongoClient } from "mongodb";
 
-async function handlerGetMeetup(req,res) {
-    if (req.method === 'GET') {
-        const client = await MongoClient.connect('mongodb+srv://admin:1234@cluster0.onkpv.mongodb.net/meetups?retryWrites=true&w=majority');
-        console.log('client', client);
-        const db = client.db();
-    
-        const meetupsCollection = db.collection('meetups');
+async function handlerGetMeetup(req, res) {
+  if (req.method === "GET") {
+    const client = await MongoClient.connect(
+      "mongodb+srv://admin:1234@cluster0.onkpv.mongodb.net/meetups?retryWrites=true&w=majority"
+    );
+    const db = client.db();
 
-        const result = await meetupsCollection.find();
-    
-    }
+    const meetupsCollection = db.collection("meetups");
+
+    const result = await meetupsCollection.find();
+  }
 }
